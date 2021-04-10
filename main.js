fd618c42d56f2e0cbb2feec38d0d808e95e0370e
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-04-09 21:37:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let edad;
+ edad= parseInt(prompt ("Ingrese su edad:"));
+ console.log(edad);
+if (edad==18) {
+    console.log(`Es mayor de edad`);
+    } else {
+    console.log(`La edad ingresada no cumple con los parámetros`);
+}
